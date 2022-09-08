f8ab8e622ee157f22f7cf67acb629a6a8e830f15
Arhun Saday <arhunsad@gmail.com>
2022-09-08 18:01:13 +0200

    Lvim: remove copilot

--- vim/.config/lvim/config.lua
+++ vim/.config/lvim/config.lua
@@ -217,20 +217,6 @@ linters.setup {
 
 -- Additional Plugins
 lvim.plugins = {
-  -- { "github/copilot.vim" },
-  -- { "zbirenbaum/copilot.lua",
-  --   event = { "VimEnter" },
-  --   config = function()
-  --     vim.defer_fn(function()
-  --       require("copilot").setup {
-  --         plugin_manager_path = get_runtime_dir() .. "/site/pack/packer",
-  --       }
-  --     end, 100)
-  --   end,
-  -- },
-  -- { "zbirenbaum/copilot-cmp",
-  --   after = { "copilot.lua", "nvim-cmp" },
-  -- },
   { "navarasu/onedark.nvim",
     config = function()
       require('onedark').setup {
@@ -271,25 +257,6 @@ lvim.plugins = {
     end
   },
   { "psliwka/vim-smoothie" },
-  -- {
-  --   "karb94/neoscroll.nvim",
-  --   event = "WinScrolled",
-  --   config = function()
-  --     require('neoscroll').setup({
-  --       -- All these keys will be mapped to their corresponding default scrolling animation
-  --       mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>',
-  --         '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-  --       hide_cursor = true, -- Hide cursor while scrolling
-  --       stop_eof = true, -- Stop at <EOF> when scrolling downwards
-  --       use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-  --       respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-  --       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-  --       easing_function = nil, -- Default easing function
-  --       pre_hook = nil, -- Function to run before the scrolling animation starts
-  --       post_hook = nil, -- Function to run after the scrolling animation ends
-  --     })
-  --   end
-  -- },
   -- {
   --   "lukas-reineke/indent-blankline.nvim",
   --   config = function()
